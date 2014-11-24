a2a7a647b62da3fe527e36d30fbba6f78260b06c
dusan <rostacik@gmail.com>
2014-11-24 16:59:32 +0100

    cleanup

---
+++ ModalWindowAsync/Scripts/Dialog/host.js
@@ -0,0 +1,55 @@
+﻿$(document).ready(function () {
+    $('#oneModalBtn').click(function () {
+        openFirstModal().done(function (val) {
+            alert(val);
+        });
+    });
+
+    $('#twoModalsBtn').click(function () {
+        openFirstModal().done(function (val) {
+            openSecondModal().done(function (val2) {
+                alert(val + " sent from first window, " + val2 + " sent from second");
+            });
+        });
+    });
+});
+
+function myOpenWindowFn(param) {
+    var deferred = $.Deferred();
+
+    var hostIframe = "<iframe id=\"" + param.iframeId + "\" src=\"" + param.url + "\" frameborder=\"0\"></iframe>";
+    var dialogDiv = $("<div id=\"dialog\" title=\"Basic dialog\">" + hostIframe + "</div >").dialog({ modal: true });
+
+    window.addEventListener("message", msgReceived, false);
+
+    function msgReceived(event) {
+        if (event && event.data) {
+            var dataObject = JSON.parse(event.data);
+
+            if ((dataObject) && (dataObject.iframeId === param.iframeId)) {
+                window.removeEventListener("message", msgReceived, false);
+                dialogDiv.dialog('close');
+
+                deferred.resolve(dataObject.result); //send response from dialog
+            }
+        }
+    }
+
+    return deferred.promise();
+}
+
+function openFirstModal() {
+    var popupRes = myOpenWindowFn({ url: 'popup.html', iframeId: 'someIframeId' });
+
+    return popupRes.then(function (value) {
+        return (value + " edited by then in openFirstModal");
+    });
+}
+
+function openSecondModal() {
+    var popupRes = myOpenWindowFn({ url: "popup2.html", iframeId: 'someIframeId2' });
+
+    return popupRes.then(function (value) {
+        return (value + " edited by then in openSecondModal");
+    });
+}
---
+++ ModalWindowAsync/Scripts/Dialog/popup2.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    $('#sendInput').click(function () {
+        var hostIfrm = window.frameElement;
+
+        if (hostIfrm) {
+            var hostIfrmId = hostIfrm.id;
+
+            window.parent.postMessage(JSON.stringify({ result: $('#textInput').val(), iframeId: hostIfrmId }), '*');
+
+            console.log(hostIfrmId);
+        }
+    });
+});
